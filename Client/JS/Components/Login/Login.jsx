937b8633f45b94d653ebe48756e9b28d1b4dc667
Adi <avinnakotaster@gmail.com>
2015-08-09 22:58:56 -0700

    [Create] is not working currently and will migrate to using CDN

--- Client/JS/Components/Login/Login.jsx
+++ Client/JS/Components/Login/Login.jsx
@@ -1,17 +1,26 @@
 var React = require('react');
-var Username = require('./Username.jsx');
-var Pass = require('./Pass.jsx');
 
 var Login = React.createClass({
-	
-	render: function() {
-		return (
-			<form id = 'Login'><h1>Login Here!</h1>
-				<Username />
-				<Pass />
-			</form>
-		);
-	}
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+    var username = React.findDOMNode(this.refs.username).value.trim();
+    var pass = React.findDOMNode(this.refs.pass).value.trim();
+    this.props.onLoginSubmit({username: username, password: pass});
+  },
+
+  render: function() {
+    return (
+        <form onSubmit ={this.handleSubmit}><h1>Login Here!</h1>
+          <input type ='text' ref ='username'/>
+          <input type = 'password' ref = "pass" />
+          <input type ="submit"/>
+        </form>
+    );
+  }
 });
+
+
+
 
 module.exports = Login;
--- Client/app.jsx
+++ Client/app.jsx
@@ -1,7 +1,44 @@
-var React = require('react');
+var Login = React.createClass({
 
-var Login = require('./js/components/Login/Login.jsx');
-var Username = require('./js/components/Login/Username.jsx');
-var Pass = require('./js/components/Login/Pass.jsx');
+  handleSubmit: function(event) {
+    event.preventDefault();
+    var username = React.findDOMNode(this.refs.username).value.trim();
+    var pass = React.findDOMNode(this.refs.pass).value.trim();
+    this.props.onLoginSubmit({username: username, password: pass});
+  },
 
-React.render(<Login />, document.getElementById('hello'));
+  render: function() {
+    return (
+        <form onSubmit ={this.handleSubmit}><h1>Login Here!</h1>
+          <input type ='text' ref ='username'/>
+          <input type = 'password' ref = "pass" />
+          <input type ="submit"/>
+        </form>
+    );
+  }
+});
+
+var LoginSubmit = React.createClass({
+  handleLoginSubmit: function(user) { 
+    $.ajax({
+      url: this.props.url, 
+      type: 'POST',
+      data: user, 
+      success: function(data) {
+            console.log('The username is: ', data.username);
+            console.log('The password is, ', data.password);
+          }.bind(this),
+            error: function() {
+              console.log(error);
+          }.bind(this)
+    });
+  },
+
+  render: function() {
+    return (
+      <Login onLoginSubmit ={this.handleLoginSubmit} />
+    );
+  }
+});
+
+React.render(<LoginSubmit url ='/login'/>, document.getElementById('test'));
