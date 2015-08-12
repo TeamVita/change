02cbb1a08018b1bde68735388737bbccee8c9d2f
Silly Knight <sillyknight1986@gmail.com>
2015-08-12 13:56:21 -0700

    [refactor] Setup new file structure, seperate models and methods

--- app.js
+++ app.js
@@ -69,6 +69,27 @@ app.use(function(err, req, res, next) {
 });
 
 // database test
-app.set('models', require('./models'));
+app.set('models', require('./db/index'));
+var Donor = app.get('models').Donor;
+// var Recipient = app.get('models').Recipient;
+// var Donation = app.get('models').Donation;
+// var Purchase = app.get('models').Purchase;
+
+// var donor = {
+//   create: function() {
+//     Donor.create({ username: 'john', password: '1111' }).then(function(result) {
+//       // result
+//       return Promise.all(result);
+//     })
+//   },
+
+//   findAllDonors: function() {
+//     Donor.findAll().then(function(donors) {
+//       donors.forEach(function(donor) {
+//         console.log(donor.get());
+//       })
+//     })
+//   }
+// }
 
 module.exports = app;
