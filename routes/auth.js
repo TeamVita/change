3aef262e71bc2574ebcf85bbff278498eb000948
Silly Knight <sillyknight1986@gmail.com>
2015-08-17 00:30:19 -0700

    [refactor] Add organization donor routing selector

--- routes/auth.js
+++ routes/auth.js
@@ -4,26 +4,39 @@ var router = express.Router();
 /* Authentication */
 
 router.get('/', function(req, res, next) {
-  res.send('Dummy message for authentication');
+  res.send("Organization/Donor");
+});
+
+// Redirect
+router.post('/', function(req, res, next) {
+
+});
+
+router.get('/org', function(req, res, next) {
+  res.send('Dummy message for organization authentication');
+});
+
+router.get('/donor', function(req, res, next) {
+  res.send('Dummy message for donor authentication');
 });
 
 // TODO: utility functions
 
 // create?
 // TODO: refactor to use dedicated auth functions
-router.post('/login', function() {
+router.post('/org/login', function() {
 
 });
 
-router.post('/signup', function() {
+router.post('/org/signup', function() {
 
 });
 
-router.get('/login', function() {
+router.get('/donor/login', function() {
 
 });
 
-router.get('/signup', function() {
+router.get('/donor/signup', function() {
 
 });
 
