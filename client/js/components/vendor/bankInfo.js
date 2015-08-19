250720b88ea4bc110282dfddc3bf9a5f31af8ce5
Vincent Rupp Jr <vincent@vaerek.net>
2015-08-19 12:02:15 -0700

    [cleanup] Correct styling and set bankInfo logic to match personalInfo

--- client/js/components/vendor/bankInfo.js
+++ client/js/components/vendor/bankInfo.js
@@ -4,33 +4,13 @@ var Promise = require('promise');
 
 var bankInfo = React.createClass({
 
-  handleSubmit: function(event) {
-
-    event.preventDefault();
-    var bankData = {};
-    for (var field in this.refs) {
-      bankData[field] = field.getDOMNode().value.trim();
-    }
-
-    var promise = new Promise(function (resolve, reject) {
-        // TODO send bankData to server
-        actions.signUp(bankData, resolve);
-      });
-    promise.then(function(resp) {
-      // render welcome
-    });
-  },
-
   render: function() {
 
     return (
-      <div id = 'form'>
-        <form onSubmit ={this.handleSubmit}><h1>Give us your bank account info.</h1>
-          <div class ='input'><input placeholder='routing' type ='text' ref ='routing' /></div>
-          <div class ='input'><input placeholder='account' type = 'text' ref ='account' /></div>
-          <input type ="submit" />
-        </form>
-      </div>
+      <h1>Give us your bank account info.</h1>
+      <div class ='input'><input placeholder='routing' type ='text' ref ='routing' /></div>
+      <div class ='input'><input placeholder='account' type = 'text' ref ='account' /></div>
+      <input type ="submit" />
     );
   }
 });
--- client/js/components/vendor/personalInfo.js
+++ client/js/components/vendor/personalInfo.js
@@ -7,14 +7,15 @@ var personalInfo = React.createClass({
   render: function() {
 
     return (
-          <div class ='input'><input placeholder='email' type ='text' ref ='email' /></div>
-          <div class ='input'><input placeholder='first_name' type = 'text' ref ='first_name' /></div>
-          <div class ='input'><input placeholder='last_name' type = 'text' ref ='last_name' /></div>
-          <div class ='input'><input placeholder='dob_day' type = 'text' ref ='dob_day' /></div>
-          <div class ='input'><input placeholder='dob_month' type = 'text' ref ='dob_month' /></div>
-          <div class ='input'><input placeholder='dob_year' type = 'text' ref ='dob_year' /></div>
-          <div class ='input'><input placeholder='company_name' type = 'text' ref ='company_name' /></div>
-          <input type ="submit" />
+      <h1>Give us your personal info.</h1>
+      <div class ='input'><input placeholder='email' type ='text' ref ='email' /></div>
+      <div class ='input'><input placeholder='first_name' type = 'text' ref ='first_name' /></div>
+      <div class ='input'><input placeholder='last_name' type = 'text' ref ='last_name' /></div>
+      <div class ='input'><input placeholder='dob_day' type = 'text' ref ='dob_day' /></div>
+      <div class ='input'><input placeholder='dob_month' type = 'text' ref ='dob_month' /></div>
+      <div class ='input'><input placeholder='dob_year' type = 'text' ref ='dob_year' /></div>
+      <div class ='input'><input placeholder='company_name' type = 'text' ref ='company_name' /></div>
+      <input type ="submit" />
     );
   }
 });
