abba3e1d8f8a034a5c27a28b905020b0b6854ac4
Silly Knight <sillyknight1986@gmail.com>
2015-08-20 15:13:45 -0700

    [fix] Resolve syntax error in account.js

--- client/js/components/shelter/controller.js
+++ client/js/components/shelter/controller.js
@@ -14,11 +14,12 @@ var Signup = React.createClass({
     event.preventDefault();
     var info = { username: "Test Recipient", password: "1234" };
     var self = this;
-    shelterActions.shelterSignUp(info, function(data) {
-      console.log("receive from server", data);
-      // TODO: refactor use bind later
-      self.setState({pane: 'orgInfo'});
-    });
+    console.log("click event!");
+    // shelterActions.shelterSignUp(info, function(data) {
+    //   console.log("receive from server", data);
+    //   // TODO: refactor use bind later
+    //   self.setState({pane: 'orgInfo'});
+    // });
   },
 
   render: function() {
