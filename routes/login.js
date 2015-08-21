6f9bc4371bff970e3c5dea7fb8aa9fc56167bbe5
Silly Knight <sillyknight1986@gmail.com>
2015-08-21 15:27:48 -0700

    [cleanup] Remove test code

--- routes/login.js
+++ routes/login.js
@@ -8,24 +8,7 @@ router.post('/donor', function(req, res) {
 });
 
 router.post('/vendor', function(req, res, next) {
-  // Vendor test to create a recipient
-  var pin = req.body.pin;
-  var amount = req.body.amount;
-  utility.createRecipient(Number(pin), "first name", "last name")
-  .then(function(recipient) {
-    console.log("Created recipient", recipient);
-  })
-  .then(function() {
-    return utility.findRecipientByPin(pin)
-  })
-  .then(function(recipient) {
-    // console.log("Find recipient", recipient);
-    // charge 
-    return recipient;
-  })
-  .then(function(recipient) {
-    res.send(recipient);
-  })
+  
 });
 
 router.post('/shelter', function(req, res) {
