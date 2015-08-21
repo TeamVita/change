691a1ae557b1c3d717410d3bc514aa8e4c2e4c33
Thomas <thomas.c.young830@gmail.com>
2015-08-20 19:59:43 -0700

    [feat] Working on landing page

---
+++ client/js/components/header.js
@@ -0,0 +1,57 @@
+var React = require('react');
+
+var Header = React.createClass({  
+  render: function() {
+    return <header className="navbar navbar-inverse white" role="banner">
+      <div className="container">
+        <div className="navbar-header">
+            <button className="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a href="index.html" className="navbar-brand">Change</a>
+        </div>
+        <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul className="nav navbar-nav navbar-right">
+           
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                    About Us
+                    <i className="fa fa-chevron-down"></i>
+                  </a>
+                  <ul className="dropdown-menu">
+          
+              <li><a href="signup.html">Sign Up & Sign In</a></li>
+              <li><a href="signup-rotate.html">Sign Up Miscellaneous</a></li>
+              <li><a href="404.html">404 Page</a></li>
+                </ul>
+              </li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                    Blog 
+                    <i className="fa fa-chevron-down"></i>
+                  </a>
+                  <ul className="dropdown-menu">
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="blogpost.html">Blog Post</a></li>
+                </ul>
+              </li>
+              <li>
+                  <a href="signup.html">Sign Up</a>
+              </li>
+            </ul>
+        </nav>
+      </div>
+      // {this.renderBody()}
+  </header>
+  },
+
+  renderBody: function() {
+    <div>
+    HIIII
+    </div>
+  }
+})
+
+module.exports = Header;
