948fb273c746028032ad2ca3ae43ebed880c5e5a
Silly Knight <sillyknight1986@gmail.com>
2015-08-21 11:56:26 -0700

    [update] Test onChange event system

--- client/js/components/shelter/signup.js
+++ client/js/components/shelter/signup.js
@@ -4,6 +4,10 @@ var Constants = require('../../constants/Constants.js');
 
 var OrgSignup = React.createClass({
 
+  getInitialState: function() {
+    return {text: "null"};
+  },
+
   getFields: function() {
     var fields = {};
     for (var keys in this.refs) {
@@ -12,12 +16,21 @@ var OrgSignup = React.createClass({
     return fields;
   },
 
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+// <div className ='input'><input placeholder='Password' ref='password' type='text'/></div>
   render: function() {
     return (
       <div id = 'form'>
         <h1>Welcome to Organization Login Page</h1>
-        <div className ='input'><input placeholder='Organization Name' ref='email' type ='text'/></div>
-        <div className ='input'><input placeholder='Password' ref='password' type='text'/></div>
+        <div className ='input'>
+          <input placeholder='Organization Name' ref='email' type ='text'/>
+        </div>
+        <div className ='input'>
+          <input placeholder="" onChange={this.onChange} type='text' ref='password' value={this.state.text}/>
+        </div>
         <input type ="submit" />
       </div>
     );
