cc52224f326280b5c1a9a640ea93da54ceb5b6f9
Vincent Rupp Jr <vincent@vaerek.net>
2015-08-24 11:07:30 -0700

    [feat] Properly route shelter signup on client

--- client/js/components/shelter/signup.js
+++ client/js/components/shelter/signup.js
@@ -5,7 +5,7 @@ var Constants = require('../../constants/Constants.js');
 var OrgSignup = React.createClass({
 
   getInitialState: function() {
-    return {text: "null"};
+    return {text: ""};
   },
 
   getFields: function() {
@@ -26,10 +26,10 @@ var OrgSignup = React.createClass({
       <div id = 'form'>
         <h1>Welcome to Shelter Signup Page</h1>
         <div className ='input'>
-          <input placeholder='Organization Name' ref='email' type ='text'/>
+          <input placeholder='organization name' ref='email' type ='text'/>
         </div>
         <div className ='input'>
-          <input placeholder="" onChange={this.onChange} type='text' ref='password' value={this.state.text}/>
+          <input placeholder="password" onChange={this.onChange} type='text' ref='password' value={this.state.text}/>
         </div>
         <input type ="submit" />
       </div>
