a586b10d937e7c37a34292b845be7414180f17ab
Vincent Rupp Jr <vincent@vaerek.net>
2015-08-24 11:30:33 -0700

    [feat] Add email input to shelter signup

--- client/js/components/shelter/signup.js
+++ client/js/components/shelter/signup.js
@@ -26,7 +26,10 @@ var OrgSignup = React.createClass({
       <div id = 'form'>
         <h1>Welcome to Shelter Signup Page</h1>
         <div className ='input'>
-          <input placeholder='organization name' ref='email' type ='text'/>
+          <input placeholder='organization name' ref='name' type ='text'/>
+        </div>
+        <div className ='input'>
+          <input placeholder='email' ref='email' type ='text'/>
         </div>
         <div className ='input'>
           <input placeholder="password" onChange={this.onChange} type='text' ref='password' value={this.state.text}/>
