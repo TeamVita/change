0faf4cbfec1fcb18b583bc6d68cc738f41022a84
Thomas <thomas.c.young830@gmail.com>
2015-08-24 19:47:56 -0700

    [feat] Landing page style

---
+++ client/js/components/threesteps.js
@@ -0,0 +1,42 @@
+<section id="portfolio">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <h2>How It Works</h2>
+                    <hr className="star-primary">
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src={"style/images/Step_1_Illustration.png"} className="img-responsive center-block" />
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src={"style/images/Step_2_Illustration.png"} className="img-responsive"/>
+                    </a>
+                </div>
+                <div className="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" className="portfolio-link" data-toggle="modal">
+                        <div className="caption">
+                            <div className="caption-content">
+                                <i className="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src={"style/images/Step_3_Illustration.png"} className="img-responsive"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
