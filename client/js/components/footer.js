a086c0bce173859952f4f221779d3144ad4a4c18
Thomas <thomas.c.young830@gmail.com>
2015-08-26 18:23:20 -0700

    [feat] Create Pin page complete

---
+++ client/js/components/footer.js
@@ -0,0 +1,26 @@
+<!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>About Us</h3>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Vendors</h3>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Shelters</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                <img src="./styles/images/Change_logo.png"/>
+                    <div class="col-lg-12">
+                    </div>
+                </div>
+            </div>
+        </div>
