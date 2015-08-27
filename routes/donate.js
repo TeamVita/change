c696a235cf1e0599b5b3428b7c6d4875ae03a30b
Vincent Rupp Jr <vincent@vaerek.net>
2015-08-26 19:08:08 -0700

    [bugfix] Fix donateToRecipientByPin so that it returns null if no PIN exists

--- routes/donate.js
+++ routes/donate.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var makePayment = require('../payment');
+var utility = require('./subroutes/utility');
 
 // Create vendor, add to DB, redirect user
 router.post('/', function(req, res) {
 	var newCharge = makePayment(req);
 	newCharge.then(function(charge) {
-		// TODO add payment to DB
-
-		res.send(charge);
+		var donation = utility.donateToRecipientByPin(req.body.pin, req.body.amt, req.body.type);
+		donation.then(function(recipient) {
+			console.log(recipient);
+			res.send(recipient);
+		});
 	});
 });
 
