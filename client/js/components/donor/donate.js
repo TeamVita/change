a303ff587efe9178d412720272ce934ac965ade0
Thomas <thomas.c.young830@gmail.com>
2015-08-28 10:13:56 -0700

    [feat] Create footer for all pages

--- client/js/components/donor/donate.js
+++ client/js/components/donor/donate.js
@@ -159,8 +159,12 @@ var Donate = React.createClass({
           </header>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         
+>>>>>>> [feat] Create footer for all pages
+=======
+
 >>>>>>> [feat] Create footer for all pages
       </div>
 
