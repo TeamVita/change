1bf64967f7b2d70df68c4b690ba3ea1c4a11955b
Thomas <thomas.c.young830@gmail.com>
2015-08-29 16:05:14 -0700

    [refactor] Add $ to form

--- client/js/components/donor/donate.js
+++ client/js/components/donor/donate.js
@@ -84,22 +84,32 @@ var Donate = React.createClass({
         <header className="fullwidth">
             <img className="logo" onClick={this.donorPage} src={"./styles/images/Change_logo.png"}/>
           </header>
+
         <div className="container">
               <h2 className="text-center formheader2">Make a Donation</h2>
 
-               <div className="row">
-                  <div className="form-group col-sm-6 col-sm-4 col-sm-offset-4 text-center floating-label-form-group controls">
-                    <span className="fonty">Pin Tag #</span>
-                      <input type="text" maxLength='4' className="text-center" placeholder="####" id="name" ref='PIN'/>
-                  </div>
-                </div>
+            <div className="row">
+            <div className="control-group">
+              <div className="form-group col-md-4 col-md-offset-4 floating-label-form-group input-group">
+                <span className="fonty input-group-addon text-center noshow">4 Digit Pil</span>
+                  <input type="" maxLength='4' className="" placeholder=""/>
+              </div>
+            </div>
 
-                <div className="row">
-                  <div className="form-group col-sm-6 col-sm-4 col-sm-offset-4 text-center floating-label-form-group controls">
-                    <span className="fonty">Amount:</span>
-                      <input type="text" onChange={this.handleChange} className="text-center" placeholder="$0.00" id="name" ref='amount'/>
-                  </div>
-                </div>
+            <div className="row control-group text-center">
+              <div className="form-group col-md-4 col-md-offset-4 floating-label-form-group controls input-group">
+                <span className="fonty input-group-addon"> Pin Tag #:</span>
+                  <input type="text" maxLength='4' className="" placeholder="####" id="name" ref='PIN'/>
+              </div>
+            </div>  
+
+            <div className="row control-group text-center">
+              <div className="form-group col-md-4 col-md-4 col-md-offset-4 floating-label-form-group controls input-group">
+                <span className="fonty input-group-addon text-center">Amount:  $</span>
+                  <input type="text" onChange={this.handleChange} className="" placeholder="0.00" id="" ref='amount'/>
+              </div>
+            </div> 
+            </div>
 
             <div className="col-lg-12 text-center">
               <h4 className="formheader">What would you like to give?</h4>
